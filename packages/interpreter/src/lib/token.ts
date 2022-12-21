93092fdee12c9eecd77ebca9b3f35eb8ea760ff0
Nipuna Gunathilake <nipuna777@gmail.com>
2022-12-21 09:41:28 +0800

    add token and token types

---
+++ packages/interpreter/src/lib/token.ts
@@ -0,0 +1,24 @@
+import { TokenType } from './tokenType';
+
+class Token {
+  type: TokenType;
+  lexeme: string;
+  literal: Record<string, string>;
+  line: number;
+
+  Token(
+    type: TokenType,
+    lexeme: string,
+    literal: Record<string, string>,
+    line: number
+  ) {
+    this.type = type;
+    this.lexeme = lexeme;
+    this.literal = literal;
+    this.line = line;
+  }
+
+  toString() {
+    return `${this.type} ${this.lexeme} ${this.literal}`;
+  }
+}
